27fe4bbe4cca36ba3c2a9b1931703b4ccc7cfcf4
AS-AlStar <starovojtov.alexander@gmail.com>
2021-09-05 23:34:30 +0300

    Add web users create

--- app/services/web/users/create.rb
+++ app/services/web/users/create.rb
@@ -1,30 +1,34 @@
 # frozen_string_literal: true
 
-module Users
-  class Create < ApplicationService
-    step :validate
-    step :save
+module Web
+  module Users
+    class Create < ApplicationService
+      step :validate
+      step :save
 
-    dependency :validator, Users::CreateValidator
-    dependency :users_repo, User
+      dependency :validator, ::Users::CreateValidator
+      dependency :users_repo, User
 
-    private
+      private
 
-    def validate(params:, **context)
-      result = validator.call(params)
+      def validate(params:, **context)
+        result = validator.call(params)
 
-      if result.success?
-        context.merge(attrs: result.data)
-      else
-        raise FailureError, result.data
+        if result.success?
+          context.merge(attrs: result.data)
+        else
+          raise FailureError, result.data
+        end
       end
-    end
 
-    def save(attrs:, **)
-      user = users_repo.create!(attrs)
-      { user: user }
-    rescue ActiveRecord::RecordNotUnique
-      raise FailureError, identity_number: :already_exists
+      def save(attrs:, **)
+        user = users_repo.new(**attrs.except(:tags))
+        user.tag_list.add(attrs.fetch(:tags))
+        user.save!
+        { user: user }
+      rescue ActiveRecord::RecordNotUnique
+        raise FailureError, identity_number: :already_exists
+      end
     end
   end
 end

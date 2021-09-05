77b14b965a1dc6c78acb28dbaa246f6fa2c68d21
AS-AlStar <starovojtov.alexander@gmail.com>
2021-09-06 00:52:25 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, format: "json" do
+    namespace :v1 do
+      resources :users, only: [:create]
+
+      namespace :accounts do
+        post :create
+        post :deposit
+        post :transfer
+      end
+
+      namespace :reports do
+        post :sum_deposit_by_currencies
+        post :sum_account_balance_by_currencies
+        post :average_max_min_transfer
+      end
+    end
+  end
 end
